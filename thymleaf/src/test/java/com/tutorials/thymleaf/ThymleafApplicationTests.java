c7af5eca9e1dd32cad7ec25a032f1f78a3c875b8
yasahmed <eessaouira@gmail.com>
2019-01-25 09:22:30 +0100

    theamleaf

---
+++ thymleaf/src/test/java/com/tutorials/thymleaf/ThymleafApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tutorials.thymleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymleafApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
